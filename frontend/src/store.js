f10628e06c771f87de7475fa440beddf31f93c68
pawan <ps9502748@gmail.com>
2024-03-16 10:36:32 +0530

    first commit

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -164,12 +164,12 @@ let inialState = {
 };
 
 const middleware = [thunk];
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   reducer,
   inialState,
-  // applyMiddleware(...middleware)
-  composeWithDevTools(applyMiddleware(...middleware))
+  applyMiddleware(...middleware)
+  // composeWithDevTools(applyMiddleware(...middleware))
 );
 export default store;
